652409728fa5b790baeaa519cd1fdcd81c512f4f
Carlos Ivan Hernandez <cihernandez162@gmail.com>
2023-06-02 02:41:18 -0500

    Format

--- server.js
+++ server.js
@@ -1,53 +1,41 @@
-'use strict';
+"use strict";
 
-const subdomain = 'SUBDOMAIN';
-const vonageNumber = 'NUMBER';
-
-const express = require('express')
+const {subdomain, vonageNumber} = require('./secrets.js');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get('/voice/answer', (req, res) => {
-  console.log('NCCO request:');
+app.get("/voice/answer", (req, res) => {
+  console.log("NCCO request:");
   console.log(`  - callee: ${req.query.to}`);
-  console.log('---');
+  console.log("---");
   res.json([
     {
-      "action": "talk",
-      "text": "Please wait while we connect you."
+      action: "talk",
+      text: "Please wait while we connect you.",
     },
     {
-      "action": "connect",
-      "from": vonageNumber,
-      "endpoint": [
-        { "type": "phone", "number": req.query.to }
-      ]
-    }
+      action: "connect",
+      from: vonageNumber,
+      endpoint: [{ type: "phone", number: req.query.to }],
+    },
   ]);
 });
 
-app.all('/voice/event', (req, res) => {
-  console.log('EVENT:');
+app.all("/voice/event", (req, res) => {
+  console.log("EVENT:");
   console.dir(req.body);
-  console.log('---');
+  console.log("---");
   res.sendStatus(200);
 });
 
-if(subdomain == "SUBDOMAIN") {
-  console.log('\n\t🚨🚨🚨 Please change the SUBDOMAIN value');
-  return false;
-}
-if(vonageNumber == "NUMBER") {
-  console.log('\n\t🚨🚨🚨 Please change the NUMBER value');
-  return false;
-}
 app.listen(3000);
 
-const localtunnel = require('localtunnel');
+const localtunnel = require("localtunnel");
 (async () => {
   const tunnel = await localtunnel({
-      subdomain: subdomain,
-      port: 3000
-    });
+    subdomain: subdomain,
+    port: 3000,
+  });
   console.log(`App available at: ${tunnel.url}`);
 })();
